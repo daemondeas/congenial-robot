5ae4a76fda90fa5f29010ba53fed85c9180899db
Andreas Svanström <andreas.svanstrom@polestar.com>
2021-12-01 18:12:45 +0100

    Add shell and first day first task

---
+++ AdventOfCode2021/AdventOfCode2021/Day1.fs
@@ -0,0 +1,11 @@
+﻿module Day1
+    let rec deeper x xs amount =
+        match xs with
+        | []    -> amount
+        | y::ys ->
+            if y > x then
+                deeper y ys (amount + 1)
+            else
+                deeper y ys amount
+
+    let firstTask args = deeper System.Int32.MaxValue args 0
---
+++ AdventOfCode2021/AdventOfCode2021/Program.fs
@@ -0,0 +1,10 @@
+﻿module Program
+
+let day1 argv =
+    let args = Array.map int argv
+    Day1.firstTask (Array.toList args)
+
+[<EntryPoint>]
+let main args =
+    printfn "%d" (day1 args)
+    0
