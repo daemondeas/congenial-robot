e0b72bd1c6f9efe4497590d96b455cba77dca387
Andreas Svanström <andreas.svanstrom@polestar.com>
2021-12-02 09:14:45 +0100

    Add day 2 input and solution

---
+++ AdventOfCode2021/AdventOfCode2021/Day2.fs
@@ -0,0 +1,53 @@
+﻿module Day2
+    type Direction =
+        | Forward
+        | Down
+        | Up
+
+    type Instruction =
+        { Direction : Direction
+          Distance  : int }
+
+    let createDirection d =
+        match d with
+        | "forward" -> Forward
+        | "down"    -> Down
+        | "up"      -> Up
+
+    let createInstruction (instr: string[]) =
+        {
+            Direction = createDirection (instr.[0])
+            Distance  = int (instr.[1])
+        }
+
+    let rec travel hor dep instructions =
+        match instructions with
+        | []    -> (hor, dep)
+        | x::xs ->
+            match x.Direction with
+            | Forward -> travel (hor + x.Distance) dep xs
+            | Down    -> travel hor (dep + x.Distance) xs
+            | Up      -> travel hor (dep - x.Distance) xs
+
+    let firstTask (args: string[]) =
+        Array.chunkBySize 2 args
+        |> Array.map createInstruction
+        |> Array.toList
+        |> travel 0 0
+        |> fun (x, y) -> x * y
+
+    let rec travelWithAim hor dep aim instructions =
+        match instructions with
+        | []    -> (hor, dep)
+        | x::xs ->
+            match x.Direction with
+            | Forward -> travelWithAim (hor + x.Distance) (dep + aim * x.Distance) aim xs
+            | Down    -> travelWithAim hor dep (aim + x.Distance) xs
+            | Up      -> travelWithAim hor dep (aim - x.Distance) xs
+
+    let secondTask (args: string[]) =
+        Array.chunkBySize 2 args
+        |> Array.map createInstruction
+        |> Array.toList
+        |> travelWithAim 0 0 0
+        |> fun (x, y) -> x * y
--- AdventOfCode2021/AdventOfCode2021/Program.fs
+++ AdventOfCode2021/AdventOfCode2021/Program.fs
@@ -4,7 +4,10 @@ let day1 argv =
     let args = Array.map int argv
     Day1.secondTask (Array.toList args)
 
+let day2 argv =
+    Day2.secondTask argv
+
 [<EntryPoint>]
 let main args =
-    printfn "%d" (day1 args)
+    printfn "%d" (day2 args)
     0
